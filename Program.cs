dea0f62af3d0326d33e4c020363d80923e299414
brennani <brennani@users.noreply.github.com>
2022-06-27 17:41:42 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+
+    class Program
+    {
+        string name = "test";
+        static void Main(string[] args)
+        {
+            Console.WriteLine(name);
+        }
+    }
+
